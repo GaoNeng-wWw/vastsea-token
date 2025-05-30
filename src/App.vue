2bc7102534e1ec98184a5b747fab0f25e6675880
GaoNeng-wWw <gaonenggg@outlook.com>
2025-05-30 21:41:13 +0800

    feat(profile): profile page

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useDark } from '@vueuse/core';
+
+useDark();
+</script>
+
+<template>
+  <div class="size-full">
+    <router-view v-slot="{ Component }">
+      <transition>
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
